52f238d33c4d442d3e6f11493e8628ae15e79851
Masood Afzal <masood.afzal.external@eviden.com>
2025-10-24 08:42:24 +0100

    Add resiliience policy with and without timeout
    Recreated PR from archived repo PR
    https://dev.azure.com/defragovuk/RWD-CPR-EPR4P-ADO/_git/ZZZ-Archive-epr-registration-validation-function-app/pullrequest/94781

--- src/EPR.RegistrationValidation.Functions/Startup.cs
+++ src/EPR.RegistrationValidation.Functions/Startup.cs
@@ -1,6 +1,10 @@
 ﻿using EPR.RegistrationValidation.Functions;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Http.Resilience;
 using Microsoft.FeatureManagement;
+using Polly;
+using Polly.Retry;
+using Polly.Timeout;
 
 [assembly: FunctionsStartup(typeof(Startup))]
 
@@ -35,22 +39,66 @@ namespace EPR.RegistrationValidation.Functions
                 var submissionApiConfig = sp.GetRequiredService<IOptions<SubmissionApiConfig>>().Value;
                 c.BaseAddress = new Uri(submissionApiConfig.BaseUrl);
                 c.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            });
+            })
+            .AddResilienceHandler("SubmissionApiResiliencePipeline", BuildResiliencePipeline());
 
             services.AddHttpClient<ICompanyDetailsApiClient, CompanyDetailsApiClient>((sp, c) =>
             {
                 var companyDetailsApiConfig = sp.GetRequiredService<IOptions<CompanyDetailsApiConfig>>().Value;
                 c.BaseAddress = new Uri(companyDetailsApiConfig.BaseUrl);
                 c.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                c.Timeout = TimeSpan.FromSeconds(companyDetailsApiConfig.Timeout);
             })
-            .AddHttpMessageHandler<CompanyDetailsApiAuthorisationHandler>();
+            .AddHttpMessageHandler<CompanyDetailsApiAuthorisationHandler>()
+            .AddResilienceHandler("CompanyDetailsResiliencePipeline", BuildResiliencePipeline<CompanyDetailsApiConfig>(o => TimeSpan.FromSeconds(o.Timeout)));
 
             services.AddScoped<ApiClients>(sp => new ApiClients
             {
                 CompanyDetailsApiClient = sp.GetRequiredService<ICompanyDetailsApiClient>(),
                 SubmissionApiClient = sp.GetRequiredService<ISubmissionApiClient>(),
             });
+        }
+
+        private static Action<ResiliencePipelineBuilder<HttpResponseMessage>> BuildResiliencePipeline() =>
+          builder => BuildResiliencePipeline(builder);
+
+        private static Action<ResiliencePipelineBuilder<HttpResponseMessage>, ResilienceHandlerContext> BuildResiliencePipeline<TConfig>(Func<TConfig, TimeSpan> timeoutSelector)
+            where TConfig : class =>
+            (builder, context) =>
+            {
+                var sp = context.ServiceProvider;
+                var timeout = timeoutSelector(sp.GetRequiredService<IOptions<TConfig>>()?.Value);
+                BuildResiliencePipeline(builder, timeout);
+            };
+
+        private static void BuildResiliencePipeline(ResiliencePipelineBuilder<HttpResponseMessage> builder, TimeSpan? timeout = null)
+        {
+            builder.AddRetry(new HttpRetryStrategyOptions
+            {
+                MaxRetryAttempts = 4,
+                Delay = TimeSpan.FromSeconds(2),
+                BackoffType = DelayBackoffType.Exponential,
+                ShouldHandle = (RetryPredicateArguments<HttpResponseMessage> args) =>
+                {
+                    bool shouldHandle;
+                    var exception = args.Outcome.Exception;
+                    if (exception is TimeoutRejectedException ||
+                       (exception is OperationCanceledException && exception.Source == "System.Private.CoreLib" && exception.InnerException is TimeoutException))
+                    {
+                        shouldHandle = true;
+                    }
+                    else
+                    {
+                        shouldHandle = HttpClientResiliencePredicates.IsTransient(args.Outcome);
+                    }
+
+                    return new ValueTask<bool>(shouldHandle);
+                },
+            });
+
+            if (timeout is not null)
+            {
+                builder.AddTimeout(timeout.Value);
+            }
         }
     }
 }
