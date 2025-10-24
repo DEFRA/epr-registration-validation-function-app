3a7fe9730ee957813f4910f32bfcb12336807002
Masood Afzal <masood.afzal.external@eviden.com>
2025-10-24 08:50:58 +0100

    fixing Unexpected error in registration file validation
    PR recreated from archived ADO repo
    https://dev.azure.com/defragovuk/RWD-CPR-EPR4P-ADO/_git/ZZZ-Archive-epr-registration-validation-function-app/pullrequest/94976

--- src/EPR.RegistrationValidation.UnitTests/Validators/OrganisationSizeTurnoverValidatorTests.cs
+++ src/EPR.RegistrationValidation.UnitTests/Validators/OrganisationSizeTurnoverValidatorTests.cs
@@ -33,6 +33,26 @@ public class OrganisationSizeTurnoverValidatorTests
         result.Errors.Count().Should().Be(errorCount);
     }
 
+    [TestMethod]
+    [DataRow(OrganisationSizeCodes.S, "X", "49.1", false, 1)]
+    [DataRow(OrganisationSizeCodes.S, "1.9", "YY", false, 1)]
+    [DataRow(OrganisationSizeCodes.S, "X", "Y", false, 1)]
+    [DataRow(OrganisationSizeCodes.L, "X", "50.1", false, 1)]
+    [DataRow(OrganisationSizeCodes.L, "1.9", "Y", false, 1)]
+    public async Task Validate_OrganisationSize_Turnover_InvalidInput(OrganisationSizeCodes organisationSize, string turnover, string totalTonnage, bool testResult, int errorCount)
+    {
+        // Arrange
+        var validator = CreateOrganisationSizeTurnoverValidator();
+        var orgDataRow = new OrganisationDataRow { OrganisationSize = organisationSize.ToString(), Turnover = turnover, TotalTonnage = totalTonnage };
+
+        // Act
+        var result = await validator.TestValidateAsync(orgDataRow);
+
+        // Assert
+        result.IsValid.Should().Be(testResult);
+        result.Errors.Count.Should().Be(errorCount);
+    }
+
     private static OrganisationSizeTurnoverValidator CreateOrganisationSizeTurnoverValidator()
     {
         return new OrganisationSizeTurnoverValidator();
